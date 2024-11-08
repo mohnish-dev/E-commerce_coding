5ca26c8ee4c59c4c078b3ae938292c542d49af63
shining-argentum <thomasjowen678@gmail.com>
2024-11-07 20:06:24 -0600

    checkIn2
    So far, I've just created the viewInventory function (and edited the main to access the function).

--- inventory.py
+++ inventory.py
@@ -3,12 +3,15 @@ import sys
 
 class Inventory:
 
+    #constructor, passes the database name
     def __init__(self, databaseName="methods.db"):
         self.databaseName = databaseName
 
+    #viewInventory function (no parameters)
     def viewInventory(self):
         print("Working on it")
 
+        #making sure we can connect to the database
         try:
             connection = sqlite3.connect(self.databaseName)
 
@@ -21,11 +24,21 @@ class Inventory:
         ## cursor to send queries through
         cursor = connection.cursor()
 
+        #SELECT query for viewing the Invetory (which is just selecting everything)
         cursor.execute("SELECT * FROM Inventory")
 
+        #actually getting the results of the query (list of tuples)
         result = cursor.fetchall()
 
-        print("Inventory Results:\n\n")
+        print("\nInventory Results:\n\n")
 
+        #outputs each item in inventory and its information in a formatted way
         for row in result:
-            print("Test")
+            print('"', row[1], '" by ', row[2], ':', sep="")
+            print("\tISBN:", row[0])
+            print("\tGenre:", row[3])
+            print("\tPages:", row[4])
+            print("\tRelease Date:", row[5])
+            print("\tStock:", row[7])
+            print("\tPrice: $", row[6], sep="")
+            print()
--- main.py
+++ main.py
@@ -62,8 +62,17 @@ def mainMenu(user, cart, inventory, history):
 
             print("Successful logout.")
 
+        # looking at inventory options
         if(option == "2"):
-            inventory.viewInventory()
+            print("Inventory Menu:") #anticipating other functions
+            print("0. View Inventory") #only one done so far
+            print()
+            i_option = input("Enter your menu choice: ")
+
+            if(i_option == "0"):
+                inventory.viewInventory()
+                #this will loop back to the main while loop afterwards; may create
+                #another while loop just for the inventory menu
 
         ## incorrect menu option
         else:
